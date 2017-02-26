a6ac3e8cb84508e55df6e08f8f670d509f8f8357
Fabian Siegel <fabians1999@gmail.com>
2017-02-26 13:25:14 +0100

    Create InstaLoad.js

---
+++ InstaLoad.js
@@ -0,0 +1,5 @@
+/* 
+*bookmarklet
+*This snippet allows to download the first picture of an instagram page; mostly effective if just one image is opened;
+*/
+javascript:location.href=document.getElementById("pImage_0").src;
